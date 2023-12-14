af0c5f08eda1f31ca88c838721e28fb8c969122a
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2023-12-13 19:41:33 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -4,12 +4,14 @@ struct ContentView: View {
     var body: some View {
         Form {
             Section(footer: Text("Made by @AppInstalleriOS")) {
-                Button {
-                    DispatchQueue.global(qos: .utility).async {
-                        
+                ForEach(try FileManager.default.contentsOfDirectory(atPath: "/var/jb/usr/lib/TweakInject") ?? [], id: \.self) { TweakName in
+                    Button {
+                        DispatchQueue.global(qos: .utility).async {
+                            InjectTweak("com.tigisoftware.Filza", "/var/jb/usr/lib/TweakInject/\(TweakName)")
+                        }
+                    } label: {
+                        Text("test")
                     }
-                } label: {
-                    Text("test")
                 }
             }
         }
