2f2b2c5dee7cd4c87504a69e0b8ced4dfa47a28f
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2023-12-16 09:50:00 -0500

    Update TSUtil.h

--- My App/TSUtil.h
+++ My App/TSUtil.h
@@ -4,6 +4,8 @@
 void spawnRoot(NSString* path, NSArray* args);
 void enumerateProcessesUsingBlock(void (^enumerator)(pid_t pid, NSString* executablePath, BOOL* stop));
 void InjectTweak(NSString* BundleID, NSString* TweakPath, NSString* RootPath);
+NSDictionary* GetInfoPlist(NSString* ExecutablePath);
+void InjectAll(NSString* RootPath);
 
 @interface LSApplicationWorkspace: NSObject
 + (instancetype)defaultWorkspace;
