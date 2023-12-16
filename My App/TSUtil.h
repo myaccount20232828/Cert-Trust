482c991b3b3095be2475747208a79cff8b1c74e7
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2023-12-16 15:09:45 -0500

    Update TSUtil.h

--- My App/TSUtil.h
+++ My App/TSUtil.h
@@ -4,6 +4,7 @@
 void spawnRoot(NSString* path, NSArray* args);
 void enumerateProcessesUsingBlock(void (^enumerator)(pid_t pid, NSString* executablePath, BOOL* stop));
 void InjectTweak(NSString* BundleID, NSString* TweakPath, NSString* RootPath);
+void InjectTweakIntoProcess(pid_t PID, NSString* TweakPath, NSString* RootPath);
 NSDictionary* GetInfoPlist(NSString* ExecutablePath);
 void InjectAll(NSString* RootPath);
 
