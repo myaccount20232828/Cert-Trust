9c8a02fbb0c83d3cdbf257a5daaba6f432510746
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 22:36:04 -0500

    Update My App.swift

--- My App/My App.swift
+++ My App/My App.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct MyApp: App {
-    //init() {
-        //dlopen("/System/Library/PrivateFrameworks/Settings/GeneralSettingsUI.framework/GeneralSettingsUI", RTLD_NOW)
-    //}
+    init() {
+        dlopen("/System/Library/PrivateFrameworks/Settings/GeneralSettingsUI.framework/GeneralSettingsUI", RTLD_NOW)
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
