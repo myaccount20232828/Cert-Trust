593d19f06fd9e670ed67cabfddbc2ca54201a167
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 23:27:05 -0500

    Update My App.swift

--- My App/My App.swift
+++ My App/My App.swift
@@ -7,7 +7,28 @@ struct MyApp: App {
     }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if let certTrustSettings = getCertTrustSettings() {
+                ZStack {
+                    ViewControllerWrapper(certTrustSettings)
+                    .frame(width: 0, height: 0)
+                    ContentView(certTrustSettings: certTrustSettings)
+                }
+            } else {
+                Text("Unable to allocate PSGCertTrustSettings")
+            }
         }
+    }
+}
+
+struct ViewControllerWrapper: UIViewControllerRepresentable {
+    var ViewController: UIViewController
+    init(_ ViewController: UIViewController) {
+        self.ViewController = ViewController
+    }
+    func makeUIViewController(context: Context) -> UIViewController {
+        return ViewController
+    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
     }
 }
