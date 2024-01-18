cd0e28ab6939f85b17a33eadcbe711cf51ebb3a5
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 23:24:24 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -1,33 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var certTrustSettings = getCertTrustSettings()
+    @State var certTrustSettings: PSGCertTrustSettings
     var body: some View {
-        if let certTrustSettings = certTrustSettings {
-            ZStack {
-                ViewControllerWrapper(certTrustSettings)
-                .frame(width: 0, height: 0)
-                Form {
-                    Button {
-                        trustCertificate("/var/mobile/Library/Filza/.Trash/cert.cer", certTrustSettings)
-                    } label: {
-                        Text("Trust 6")
-                    }
-                }
+        Form {
+            Button {
+                trustCertificate("/var/mobile/Library/Filza/.Trash/cert.cer", certTrustSettings)
+            } label: {
+                Text("Trust 6")
             }
         }
-    }
-}
-
-struct ViewControllerWrapper: UIViewControllerRepresentable {
-    var ViewController: UIViewController
-    init(_ ViewController: UIViewController) {
-        self.ViewController = ViewController
-    }
-    func makeUIViewController(context: Context) -> UIViewController {
-        return ViewController
-    }
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-        
     }
 }
