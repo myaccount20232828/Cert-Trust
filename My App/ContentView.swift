947296e00fbe058e3b140ce3330e066f763725d0
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-18 00:45:46 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -6,8 +6,15 @@ struct ContentView: View {
         Form {
             Button {
                 trustCertificate(CertificatePath, !isCertificateTrusted(CertificatePath))
+                //Auto trust
+                if let Alert = UIApplication.shared.windows.first?.rootViewController.presentedViewController as? UIAlertController {
+                    UIPasteboard.general.string = "good"
+                    Alert.actions[1].trigger()
+                } else {
+                    UIPasteboard.general.string = "bad"
+                }
             } label: {
-                Text("Set Trust")
+                Text("Set Trust 1")
             }
         }
     }
