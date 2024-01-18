36d0423958be1c5bb160548a4cc5138c6999e549
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 22:52:30 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -1,14 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var certTrustSettings = getCertTrustSettings()
     var body: some View {
         Form {
             Button {
                 //DispatchQueue.global(qos: .utility).async {
-                    getCertTrustSettings()
+                    UIApplication.shared.windows.first { $0.isKeyWindow }?.rootViewController?.present(certTrustSettings, animated: true)
                 //}
             } label: {
-                Text("Trust")
+                Text("Trust 2")
             }
         }
     }
