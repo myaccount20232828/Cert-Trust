ffd7db9b3a9e08fdb03acc66e0d826072d0873dc
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 23:40:31 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -1,12 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var ShouldTrust = true
     var body: some View {
         Form {
+            Toggle("Should Trust", isOn: $ShouldTrust)
             Button {
-                trustCertificate("/var/mobile/Library/Filza/.Trash/cert.cer", true)
+                trustCertificate("/var/mobile/Library/Filza/.Trash/cert.cer", ShouldTrust)
             } label: {
-                Text("Trust 6")
+                Text("Set Trust")
             }
         }
     }
