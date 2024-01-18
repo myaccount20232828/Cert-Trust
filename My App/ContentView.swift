3b683145c68cd12f87d62bb40faa1adde06dc76e
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 23:54:25 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -5,6 +5,7 @@ struct ContentView: View {
     var body: some View {
         Form {
             Toggle("Should Trust", isOn: $ShouldTrust)
+            Text(isCertificateTrusted("/var/mobile/Library/Filza/.Trash/cert.cer") ? "true" : "false")
             Button {
                 trustCertificate("/var/mobile/Library/Filza/.Trash/cert.cer", ShouldTrust)
             } label: {
