f396b1ba926a65626bbed91b01658767224fc79c
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 21:45:05 -0500

    Update and rename TSUtil.m to Utilities.m

---
+++ My App/Utilities.m
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+#import "Utilities.h"
+
+- (SecCertificateRef)getCertificateAtPath(NSString* certificatePath) {
+    NSData* certificateData = [NSData dataWithContentsOfFile: certificatePath];
+    if (certificateData == NULL) {
+        return NULL;
+    }
+    SecCertificateRef certificateRef = SecCertificateCreateWithData(kCFAllocatorMalloc, (__bridge CFDataRef)certificateData);
+    return certificateRef;
+}
+
+- (void)trustCertificate(NSString* certificatePath, PSGCertTrustSettings* certTrustSettings) {
+    SecCertificateRef certificateRef = getCertificateAtPath(certificatePath);
+    if (certificateRef == NULL) {
+        return;
+    }
+    [certTrustSettings setFullTrustEnabled: @YES forSpecifier: [certTrustSettings specifierForTrustSettings: certificateRef isRestricted: false]];
+}
