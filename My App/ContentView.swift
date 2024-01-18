083b352ef3f0eef9269c2799367aafcb403df4e8
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 22:56:12 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -6,7 +6,9 @@ struct ContentView: View {
         Form {
             Button {
                 //DispatchQueue.global(qos: .utility).async {
+                if let certTrustSettings = certTrustSettings {
                     UIApplication.shared.windows.first { $0.isKeyWindow }?.rootViewController?.present(certTrustSettings, animated: true)
+                }
                 //}
             } label: {
                 Text("Trust 2")
