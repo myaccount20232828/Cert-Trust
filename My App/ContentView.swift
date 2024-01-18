49623e41d2335ccb8dedc30fdbb63ec9b367b7bc
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 22:46:26 -0500

    Update ContentView.swift

--- My App/ContentView.swift
+++ My App/ContentView.swift
@@ -5,7 +5,7 @@ struct ContentView: View {
         Form {
             Button {
                 //DispatchQueue.global(qos: .utility).async {
-                    
+                    getCertTrustSettings()
                 //}
             } label: {
                 Text("Trust")
