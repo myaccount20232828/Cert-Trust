5b67acd5be533ba59b3ab3318bde634adfa73157
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-01-17 21:40:15 -0500

    Update Utilities.h

--- My App/Utilities.h
+++ My App/Utilities.h
@@ -1,7 +1,8 @@
 #import <Foundation/Foundation.h>
 
--(SecCertificateRef*)getCertificateAtPath(NSString* certificatePath);
-
+- (SecCertificateRef)getCertificateAtPath(NSString* certificatePath);
+- (void)trustCertificate(NSString* certificatePath, PSGCertTrustSettings* certTrustSettings);
+  
 @interface PSGCertTrustSettings: UIViewController
 - (id)specifierForTrustSettings:(SecCertificateRef)arg1 isRestricted:(BOOL)arg2;
 - (void)setFullTrustEnabled:(id)arg1 forSpecifier:(id)arg2;
